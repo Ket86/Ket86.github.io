fd70f2a2bcf9f7e376669759c1f2cbafca5c1d9a
Ket86 <aprile.katerina@gmail.com>
2020-12-03 19:36:21 +0200

    add project premium

---
+++ project premium/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+$(document).ready(function(){
+    $('.slider-main-plugin').slick({
+        arrows: false,
+        dots: true,
+    });
+
+  $('.slider-product-plugin').slick({
+    arrows: true,
+    dots: false,
+});
+});
